7ceb62e2381ea80b0aafdcc07bf66ca370825dbd
Alan <qwedsa274@gmail.com>
2019-05-19 15:24:14 +0800

    20190519 1523 DB connect

--- BookListRazor/Program.cs
+++ BookListRazor/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.Server.Kestrel;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 namespace BookListRazor
