7e6622b9bb9334aca06689dcb6219450e47fd6f7
Alan <qwedsa274@gmail.com>
2019-05-20 15:53:14 +0800

    201905201552 Create Index and Create book btn

---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
